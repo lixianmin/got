2d93358e6933e0ace991487230e97310e9b6d89a
lixianmin <lixianmin@live.cn>
2020-06-13 09:00:38 +0800

    加入convert

---
+++ convert/convert.go
@@ -0,0 +1,85 @@
+package convert
+
+import (
+	"encoding/json"
+	"fmt"
+	"strconv"
+	"unsafe"
+)
+
+/********************************************************************
+created:    2018-09-05
+author:     lixianmin
+
+Copyright (C) - All Rights Reserved
+*********************************************************************/
+
+func ToString(v interface{}) string {
+	switch v := v.(type) {
+	case []byte:
+		return *(*string)(unsafe.Pointer(&v))
+	case int:
+		return strconv.FormatInt(int64(v), 10)
+	case int16:
+		return strconv.FormatInt(int64(v), 10)
+	case int32:
+		return strconv.FormatInt(int64(v), 10)
+	case int64:
+		return strconv.FormatInt(v, 10)
+	case uint:
+		return strconv.FormatUint(uint64(v), 10)
+	case uint16:
+		return strconv.FormatUint(uint64(v), 10)
+	case uint32:
+		return strconv.FormatUint(uint64(v), 10)
+	case uint64:
+		return strconv.FormatUint(v, 10)
+	case float64:
+		return strconv.FormatFloat(v, 'f', -1, 64)
+	case float32:
+		return strconv.FormatFloat(float64(v), 'f', -1, 32)
+	case string:
+		return v
+	default:
+		return fmt.Sprintf("convert.ToString(): unexpected type for ToString, got type %T", v)
+	}
+}
+
+var _toJson = json.Marshal
+var _fromJson = json.Unmarshal
+
+func InitJson(toJson func(v interface{}) ([]byte, error), fromJson func(data []byte, v interface{}) error) {
+	if toJson == nil {
+		panic("toJson is nil")
+	}
+
+	if fromJson == nil {
+		panic("fromJson is nil.")
+	}
+
+	_toJson = toJson
+	_fromJson = fromJson
+}
+
+func ToJson(v interface{}) ([]byte, error) {
+	return _toJson(v)
+}
+
+func FromJson(data []byte, v interface{}) error {
+	return _fromJson(data, v)
+}
+
+func ToHuman(num uint64) string {
+	if num >= 1073741824 {
+		var v = float64(num) / 1073741824
+		return fmt.Sprintf("%.2fG", v)
+	} else if num >= 1048576 {
+		var v = float64(num) / 1048576
+		return fmt.Sprintf("%.2fM", v)
+	} else if num >= 1024 {
+		var v = float64(num) / 1024
+		return fmt.Sprintf("%.2fK", v)
+	} else {
+		return fmt.Sprintf("%dB", num)
+	}
+}
