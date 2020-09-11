dfe27372fd82cf84a3883cc7e599961f2d5b4047
lixianmin <lixianmin@live.cn>
2020-09-11 23:14:53 +0800

    refactor

--- loom/timingwheel.go
+++ loom/timingwheel.go
@@ -59,8 +59,8 @@ func (wheel *TimingWheel) Close() error {
 }
 
 func (wheel *TimingWheel) After(timeout time.Duration) <-chan struct{} {
-	if timeout >= wheel.maxTimeout {
-		panic("timeout too much, over max timeout")
+	if timeout < 0 || timeout >= wheel.maxTimeout {
+		panic("timeout < 0, or >= maxTimeout")
 	}
 
 	var index = int(timeout / wheel.interval)
@@ -70,6 +70,7 @@ func (wheel *TimingWheel) After(timeout time.Duration) <-chan struct{} {
 
 	var position = int(atomic.LoadInt64(&wheel.position))
 	index = (position + index) % wheel.bucketsSize
+	// 由于缺少lock控制，这里有可能取到已经被关闭的chan，但这没有关系，已经关闭的说明时刻已经过去了，立即返回就好
 	var waitChan = (*wheelChan)(atomic.LoadPointer(&wheel.channels[index])).c
 
 	return waitChan
@@ -93,9 +94,14 @@ func (wheel *TimingWheel) goLoop(later Later) {
 func (wheel *TimingWheel) onTicker() {
 	var position = int(atomic.LoadInt64(&wheel.position))
 	var lastChan = (*wheelChan)(atomic.LoadPointer(&wheel.channels[position])).c
+
+	// 修改chan
 	atomic.StorePointer(&wheel.channels[position], unsafe.Pointer(&wheelChan{c: make(chan struct{})}))
 
+	// 修改position
 	position = (position + 1) % wheel.bucketsSize
 	atomic.StoreInt64(&wheel.position, int64(position))
+
+	// 关闭lastChan
 	close(lastChan)
 }
