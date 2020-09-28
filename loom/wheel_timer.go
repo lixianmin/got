cbca055d3c5f9b506e5b5a2e4edc951ee09170b2
lixianmin <lixianmin@live.cn>
2020-09-28 11:10:12 +0800

    wheel_timer支持Reset()是传入一个nextInterval

--- loom/wheel_timer.go
+++ loom/wheel_timer.go
@@ -18,7 +18,12 @@ type WheelTimer struct {
 }
 
 // Reset()方法一定是在 <- timer.C 之后调用的，因此一定是在同一个 goroutine中，不存在竟态条件问题
-func (my *WheelTimer) Reset() {
-	var data = my.wheel.fetchWheelData(my.interval)
+func (my *WheelTimer) Reset(nextInterval ...time.Duration) {
+	var interval = my.interval
+	if len(nextInterval) > 0 && nextInterval[0] >= my.wheel.step {
+		interval = nextInterval[0]
+	}
+
+	var data = my.wheel.fetchWheelData(interval)
 	my.C = data.c
 }
