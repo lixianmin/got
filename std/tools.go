146090544647427583e2dfdc268ba031688780b0
lixianmin <lixianmin@live.cn>
2020-10-14 16:44:45 +0800

    加入IsNil()方法

---
+++ std/tools.go
@@ -0,0 +1,24 @@
+package std
+
+/********************************************************************
+created:    2020-10-14
+author:     lixianmin
+
+Copyright (C) - All Rights Reserved
+*********************************************************************/
+
+import "reflect"
+
+// 方法中传入指针时，直接使用 if i == nil {} 是无法判断是不为nil的
+func IsNil(i interface{}) bool {
+	if i == nil {
+		return true
+	}
+
+	switch reflect.TypeOf(i).Kind() {
+	case reflect.Ptr, reflect.Map, reflect.Array, reflect.Chan, reflect.Slice:
+		return reflect.ValueOf(i).IsNil()
+	}
+
+	return false
+}
