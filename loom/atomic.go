b2840ab7a80770d38e37a0994822765a1c39da4d
lixianmin <lixianmin@live.cn>
2020-11-12 14:49:35 +0800

    后续基本数据类型的原子操作建议使用 https://github.com/uber-go/atomic

--- loom/atomic.go
+++ loom/atomic.go
@@ -2,7 +2,6 @@ package loom
 
 import (
 	"sync/atomic"
-	"unsafe"
 )
 
 /********************************************************************
@@ -32,32 +31,33 @@ func AddIf64(addr *int64, delta int64, predicate func(old int64) bool) bool {
 	}
 }
 
-func LoadString(addr **string) string {
-	// 最内层的unsafe.Pointer(addr)对第三方指针的包装，相当时强制类型转换，是固定写法
-	var p = (*string)(atomic.LoadPointer((*unsafe.Pointer)(unsafe.Pointer(addr))))
-	if p != nil {
-		return *p
-	}
-
-	return ""
-}
-
-func StoreString(addr **string, s string) string {
-	atomic.StorePointer((*unsafe.Pointer)(unsafe.Pointer(addr)), unsafe.Pointer(&s))
-	return s
-}
-
-func LoadBool(addr *int32) bool {
-	var v = atomic.LoadInt32(addr)
-	return v != 0
-}
-
-func StoreBool(addr *int32, b bool) bool {
-	var v int32 = 0
-	if b {
-		v = 1
-	}
-
-	atomic.StoreInt32(addr, v)
-	return b
-}
+// 后续基本数据类型的原子操作建议使用 https://github.com/uber-go/atomic ，不再提供支持
+//func LoadString(addr **string) string {
+//	// 最内层的unsafe.Pointer(addr)对第三方指针的包装，相当时强制类型转换，是固定写法
+//	var p = (*string)(atomic.LoadPointer((*unsafe.Pointer)(unsafe.Pointer(addr))))
+//	if p != nil {
+//		return *p
+//	}
+//
+//	return ""
+//}
+//
+//func StoreString(addr **string, s string) string {
+//	atomic.StorePointer((*unsafe.Pointer)(unsafe.Pointer(addr)), unsafe.Pointer(&s))
+//	return s
+//}
+//
+//func LoadBool(addr *int32) bool {
+//	var v = atomic.LoadInt32(addr)
+//	return v != 0
+//}
+//
+//func StoreBool(addr *int32, b bool) bool {
+//	var v int32 = 0
+//	if b {
+//		v = 1
+//	}
+//
+//	atomic.StoreInt32(addr, v)
+//	return b
+//}
