62e1b6f287bdb89d9508a1bec585ab1064f4a155
lixianmin01 <lixianmin01@baidu.com>
2021-10-15 10:16:39 +0800

    加入Midnight()，把时间按天对齐

--- timex/time.go
+++ timex/time.go
@@ -9,11 +9,18 @@ author:     lixianmin
 Copyright (C) - All Rights Reserved
 *********************************************************************/
 
-// 按照国人习惯的方式格式化了一下时间
+// FormatTime 按照国人习惯的方式格式化了一下时间
 func FormatTime(t time.Time) string {
 	if t.Location() != time.Local {
 		t = t.In(time.Local)
 	}
 
 	return t.Format(Layout)
+}
+
+// Midnight 把时间对齐到午夜
+func Midnight(t time.Time) time.Time {
+	var year, month, day = t.Date()
+	var midnight = time.Date(year, month, day, 0, 0, 0, 0, t.Location())
+	return midnight
 }
