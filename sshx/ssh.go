ccb89d76366d83efab6b19902e6b7be08ae020f8
lixianmin01 <lixianmin01@baidu.com>
2021-11-25 10:27:03 +0800

    将address改名为hostname

--- sshx/ssh.go
+++ sshx/ssh.go
@@ -18,9 +18,9 @@ type SSH struct {
 }
 
 // NewSSH 创建一条ssh命令。注：1. format只用于格式化参数部分，不带ssh 2. format命令中间以空格分隔
-func NewSSH(address, format string, a ...interface{}) *SSH {
-	if address == "" {
-		panic("address is empty")
+func NewSSH(hostname, format string, a ...interface{}) *SSH {
+	if hostname == "" {
+		panic("hostname is empty")
 	}
 
 	if format == "" {
@@ -29,7 +29,7 @@ func NewSSH(address, format string, a ...interface{}) *SSH {
 
 	// -tt 可避免 Pseudo-terminal will not be allocated because stdin is not a terminal
 	// -o StrictHostKeyChecking=no  可避免 The authenticity of host 'xxx' can't be established. RSA key fingerprint is xxx. Are you sure you want to continue connecting (yes/no)
-	var text = address + " -tt -o StrictHostKeyChecking=no " + fmt.Sprintf(format, a...)
+	var text = hostname + " -tt -o StrictHostKeyChecking=no " + fmt.Sprintf(format, a...)
 	var args = strings.Split(text, " ")
 	var my = &SSH{args: args}
 	return my
