ffcb070b3c5c92610b502287224b294ba89f7c51
lixianmin01 <lixianmin01@baidu.com>
2021-11-25 10:25:50 +0800

    修改sshx逻辑，将address作为最一个参数

--- sshx/ssh.go
+++ sshx/ssh.go
@@ -17,14 +17,22 @@ type SSH struct {
 	args []string
 }
 
-// NewSSH 创建一条ssh命令。1. format只用于格式化参数部分，不带ssh 2. format命令中间以空格分隔
-func NewSSH(format string, a ...interface{}) *SSH {
+// NewSSH 创建一条ssh命令。注：1. format只用于格式化参数部分，不带ssh 2. format命令中间以空格分隔
+func NewSSH(address, format string, a ...interface{}) *SSH {
+	if address == "" {
+		panic("address is empty")
+	}
+
+	if format == "" {
+		panic("format is empty")
+	}
+
 	// -tt 可避免 Pseudo-terminal will not be allocated because stdin is not a terminal
 	// -o StrictHostKeyChecking=no  可避免 The authenticity of host 'xxx' can't be established. RSA key fingerprint is xxx. Are you sure you want to continue connecting (yes/no)
-	var text = "-tt -o StrictHostKeyChecking=no " + fmt.Sprintf(format, a...)
+	var text = address + " -tt -o StrictHostKeyChecking=no " + fmt.Sprintf(format, a...)
 	var args = strings.Split(text, " ")
-	var cmd = &SSH{args: args}
-	return cmd
+	var my = &SSH{args: args}
+	return my
 }
 
 func (my *SSH) Run() ([]byte, error) {
