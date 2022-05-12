e7fecc633591467fef611dc752667b862c5c0dd3
lixianmin01 <lixianmin01@xmli.local>
2022-05-12 16:57:48 +0800

    refactor

--- webx/web.go
+++ webx/web.go
@@ -27,10 +27,11 @@ func Request(ctx context.Context, method string, url string, options ...Option) 
 
 	// 讲道理, 无论是在http.Client{}中设置Timeout, 还是设置具备超时的ctx, 都是起作用的. 实测发现它们返回的err不一样
 	// 现在遇到的问题是: 在设置http.Client{}的Timeout时, 请求仍然可能会超过很长时间, 因此现在尝试使用ctx的方案, 看看是否可以避免这个问题
-	var ctx1, cancel = context.WithTimeout(ctx, opts.Timeout)
-	defer cancel()
+	// 实测不解决问题, 还是会超时
+	//var ctx1, cancel = context.WithTimeout(ctx, opts.Timeout)
+	//defer cancel()
 
-	request, err1 := http.NewRequestWithContext(ctx1, method, url, nil)
+	request, err1 := http.NewRequestWithContext(ctx, method, url, nil)
 	if err1 != nil {
 		return nil, err1
 	}
