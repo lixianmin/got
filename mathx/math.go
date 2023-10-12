01aecfeb9919b6a3c20a43526f9b75e485b8e278
lixianmin <lixianmin@gmail.com>
2023-10-12 11:06:52 +0800

    rewrite ClampI64 and so on with generics

--- mathx/math.go
+++ mathx/math.go
@@ -1,5 +1,9 @@
 package mathx
 
+import (
+	"cmp"
+)
+
 /********************************************************************
 created:    2019-06-19
 author:     lixianmin
@@ -7,47 +11,7 @@ author:     lixianmin
 Copyright (C) - All Rights Reserved
 *********************************************************************/
 
-func ClampI32(v, min, max int32) int32 {
-	if v < min {
-		return min
-	} else if v > max {
-		return max
-	} else {
-		return v
-	}
-}
-
-func ClampI64(v, min, max int64) int64 {
-	if v < min {
-		return min
-	} else if v > max {
-		return max
-	} else {
-		return v
-	}
-}
-
-func ClampI(v, min, max int) int {
-	if v < min {
-		return min
-	} else if v > max {
-		return max
-	} else {
-		return v
-	}
-}
-
-func ClampF32(v, min, max float32) float32 {
-	if v < min {
-		return min
-	} else if v > max {
-		return max
-	} else {
-		return v
-	}
-}
-
-func ClampF64(v, min, max float64) float64 {
+func Clamp[T cmp.Ordered](v, min, max T) T {
 	if v < min {
 		return min
 	} else if v > max {
